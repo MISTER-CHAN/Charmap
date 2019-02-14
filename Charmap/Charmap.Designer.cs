aa337420194f32e9864d741627c9aff2043304ca
Chan Tsea-tow <MISTER_CHAN@outlook.com>
2019-02-14 13:40:52 +0800

    添加项目文件。

---
+++ Charmap/Charmap.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Charmap
+{
+    partial class Charmap
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvCharmap = new System.Windows.Forms.ListView();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.prevPage = new System.Windows.Forms.ToolStripMenuItem();
+            this.nextPage = new System.Windows.Forms.ToolStripMenuItem();
+            this.content = new System.Windows.Forms.ToolStripComboBox();
+            this.tsmiGoto = new System.Windows.Forms.ToolStripMenuItem();
+            this.copy = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.status = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip.SuspendLayout();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lvCharmap
+            // 
+            this.lvCharmap.Font = new System.Drawing.Font("MISTER_CHAN", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lvCharmap.Location = new System.Drawing.Point(12, 27);
+            this.lvCharmap.MultiSelect = false;
+            this.lvCharmap.Name = "lvCharmap";
+            this.lvCharmap.Size = new System.Drawing.Size(776, 398);
+            this.lvCharmap.TabIndex = 0;
+            this.lvCharmap.UseCompatibleStateImageBehavior = false;
+            this.lvCharmap.SelectedIndexChanged += new System.EventHandler(this.LvCharmap_SelectedIndexChanged);
+            this.lvCharmap.DoubleClick += new System.EventHandler(this.LvCharmap_DoubleClick);
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.prevPage,
+            this.nextPage,
+            this.content,
+            this.tsmiGoto,
+            this.copy});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip.TabIndex = 1;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // prevPage
+            // 
+            this.prevPage.Name = "prevPage";
+            this.prevPage.Size = new System.Drawing.Size(31, 20);
+            this.prevPage.Text = "<<";
+            this.prevPage.Click += new System.EventHandler(this.PrevPage_Click);
+            // 
+            // nextPage
+            // 
+            this.nextPage.Name = "nextPage";
+            this.nextPage.Size = new System.Drawing.Size(31, 20);
+            this.nextPage.Text = ">>";
+            this.nextPage.Click += new System.EventHandler(this.NextPage_Click);
+            // 
+            // content
+            // 
+            this.content.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.content.Items.AddRange(new object[] {
+            "0 ~ FFF",
+            "1000 ~ 1FFF",
+            "2000 ~ 2FFF",
+            "3000 ~ 3FFF",
+            "4000 ~ 9FFF",
+            "A000 ~ AFFF",
+            "B000 ~ EFFF",
+            "F000 ~ FFFF",
+            "10000 ~ 10FFFF"});
+            this.content.Name = "content";
+            this.content.Size = new System.Drawing.Size(75, 20);
+            this.content.SelectedIndexChanged += new System.EventHandler(this.Content_SelectedIndexChanged);
+            // 
+            // tsmiGoto
+            // 
+            this.tsmiGoto.Name = "tsmiGoto";
+            this.tsmiGoto.Size = new System.Drawing.Size(41, 20);
+            this.tsmiGoto.Text = "轉到";
+            this.tsmiGoto.Click += new System.EventHandler(this.Goto_Click);
+            // 
+            // copy
+            // 
+            this.copy.Name = "copy";
+            this.copy.Size = new System.Drawing.Size(41, 20);
+            this.copy.Text = "複製";
+            this.copy.Click += new System.EventHandler(this.Copy_Click);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.status});
+            this.statusStrip.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip.TabIndex = 2;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // status
+            // 
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(12, 17);
+            this.status.Text = "0";
+            // 
+            // Charmap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.lvCharmap);
+            this.Controls.Add(this.menuStrip);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip;
+            this.MaximizeBox = false;
+            this.Name = "Charmap";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Charmap";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvCharmap;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem prevPage;
+        private System.Windows.Forms.ToolStripMenuItem nextPage;
+        private System.Windows.Forms.ToolStripMenuItem tsmiGoto;
+        private System.Windows.Forms.ToolStripMenuItem copy;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel status;
+        private System.Windows.Forms.ToolStripComboBox content;
+    }
+}
+
---
+++ Charmap/Charmap.cs
@@ -0,0 +1,175 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Charmap
+{
+    public partial class Charmap : Form
+    {
+        private int first = 0;
+
+        public Charmap()
+        {
+            InitializeComponent();
+            content.SelectedIndex = 0;
+            ShowCharacters();
+        }
+
+        private void Content_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (first < Convert.ToInt32(content.Text.Substring(0, content.Text.IndexOf(' ')), 0x10) ||
+                first > Convert.ToInt32(content.Text.Substring(content.Text.LastIndexOf(' ') + 1), 0x10))
+            {
+                first = Convert.ToInt32(content.Text.Substring(0, content.Text.IndexOf(' ')), 0x10);
+                ShowCharacters();
+            }
+        }
+
+        private void Copy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(lvCharmap.SelectedItems[0].Text);
+        }
+
+        private void Goto_Click(object sender, EventArgs e)
+        {
+            string s = Interaction.InputBox("轉到");
+            if (s == "")
+            {
+                return;
+            }
+            if (!new Regex("^[0-9a-f]+$").IsMatch(s))
+            {
+                s = Unicode.Encode(s)[0];
+            }
+            first = Convert.ToInt32(s, 16) / 0x100 * 0x100;
+            ShowCharacters();
+            lvCharmap.Items[Convert.ToInt32(s, 16) % 0x100].Selected = true;
+            lvCharmap.Focus();
+        }
+
+        private void LvCharmap_DoubleClick(object sender, EventArgs e)
+        {
+            Clipboard.SetText(lvCharmap.SelectedItems[0].Text);
+        }
+
+        private void LvCharmap_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lvCharmap.SelectedItems.Count > 0)
+            {
+                status.Text = (first + lvCharmap.SelectedItems[0].Index).ToString("X");
+            }
+        }
+
+        private void NextPage_Click(object sender, EventArgs e)
+        {
+            if (first < 0x1000000)
+            {
+                first += 0x100;
+            }
+            ShowCharacters();
+        }
+
+        private void PrevPage_Click(object sender, EventArgs e)
+        {
+            if (first > 0x0)
+            {
+                first -= 0x100;
+            }
+            ShowCharacters();
+        }
+
+        private void ShowCharacters()
+        {
+            lvCharmap.Clear();
+            for (int i = first; i < first + 0x100; i++)
+            {
+                lvCharmap.Items.Add(Unicode.Decode(i));
+            }
+            if (first < Convert.ToInt32(content.Text.Substring(0, content.Text.IndexOf(' ')), 0x10) ||
+                first > Convert.ToInt32(content.Text.Substring(content.Text.LastIndexOf(' ') + 1), 0x10))
+            {
+                content.Text = (first / 0x1000 * 0x1000).ToString("X") + " ~ " + (first / 0x1000 * 0x1000 + 0xfff).ToString("X");
+            }
+        }
+    }
+
+    class Unicode
+    {
+        public static string Decode(int unicode)
+        {
+            string bin = Convert.ToString(unicode, 2);
+            List<byte> bytes = new List<byte>();
+            if (unicode <= 0x7f)
+            {
+                bytes.Add(Convert.ToByte("0" + bin.PadLeft(7, '0'), 2));
+            }
+            else
+            {
+                byte length = 0;
+                if (0x80 <= unicode && unicode <= 0x7ff)
+                {
+                    length = 2;
+                }
+                else if (0x800 <= unicode && unicode <= 0xffff)
+                {
+                    length = 3;
+                }
+                else if (0x10000 <= unicode && unicode <= 0x1fffff)
+                {
+                    length = 4;
+                }
+                else if (0x200000 <= unicode && unicode <= 0x3ffffff)
+                {
+                    length = 5;
+                }
+                else if (0x4000000 <= unicode && unicode <= 0x7fffffff)
+                {
+                    length = 6;
+                }
+                bin = bin.PadLeft(length * 5 + 1, '0');
+                bytes.Add(Convert.ToByte("0".PadLeft(length + 1, '1') + bin.Substring(0, 7 - length), 2));
+                bin = bin.Substring(7 - length);
+                for (int i = 0; i < length - 1; i++)
+                {
+                    bytes.Add(Convert.ToByte("10" + bin.Substring(i * 6, 6), 2));
+                }
+            }
+            return string.Join("", Encoding.UTF8.GetChars(bytes.ToArray()));
+        }
+
+        public static string[] Encode(string s)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(s);
+            List<string> unicodes = new List<string>();
+            foreach (byte b in bytes)
+            {
+                string bin = Convert.ToString(b, 2).PadLeft(8, '0');
+                if (bin.Substring(0, 1) == "0")
+                {
+                    unicodes.Add(bin.Substring(1));
+                }
+                else if (bin.Substring(0, 2) == "10")
+                {
+                    unicodes[unicodes.ToArray().Length - 1] += bin.Substring(2);
+                }
+                else
+                {
+                    unicodes.Add(bin.Substring(bin.IndexOf('0') + 1));
+                }
+            }
+            for (int i = 0; i < unicodes.Count; i++)
+            {
+                unicodes[i] = Convert.ToInt32(unicodes[i], 2).ToString("X");
+            }
+            return unicodes.ToArray();
+        }
+    }
+}
